530e4a5ec4f1f73d0ad5fa06c4f858ef0435b241
Ayumu Nono <ayumu.bete@gmail.com>
2019-06-13 11:20:21 +0900

    Revert "はじめてのDjango Part4"
    This reverts commit 36ef553b1d9f72e429aa38d1f7fae935210cf208.

--- django-tutorial2/mysite/polls/views.py
+++ django-tutorial2/mysite/polls/views.py
@@ -1,17 +1,11 @@
 from django.http import HttpResponse
-from django.http import Http404
-from .models import Question
-from django.template import loader
-from django.shortcuts import render
+
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
