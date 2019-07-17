5e837d3f9837edddc8112f301708f6bde0ab810a
Ayumu Nono <ayumu.bete@gmail.com>
2019-07-17 12:15:36 +0900

    検索画面制作取り掛かり

--- spermbank/spermbankproject/donors/forms.py
+++ spermbank/spermbankproject/donors/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Donor
 from .models import HAIR
+from .models import HEIGHT
 
 class DonorForms(forms.ModelForm):
     class Meta:
@@ -8,10 +9,7 @@ class DonorForms(forms.ModelForm):
         fields = ('number','hair_color','height','weight','ICI_IUI','blood_type','today_photo','profile','eye_color','mot','memo')
 
 
-# まずは髪色を検索できることから
-HAIR = HAIR + (('','-------')) #空欄もありにするので。
-
-class DonorSreachForm(forms.Form):
-    hair_color = forms.ChoiceField(label='髪の色', choices=HAIR, required=False)
-
-
+class DonorSreachForm(forms.ModelForm):
+    class Meta:
+        model = Donor
+        fields = ('hair_color','height')
--- spermbank/spermbankproject/donors/views.py
+++ spermbank/spermbankproject/donors/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView,ListView,CreateView
-
+from django.views import generic
 from .models import Donor
-from .forms import DonorForms
+from .forms import DonorForms,DonorSreachForm
+
+from django.db.models import Q
+import logging
+
+from django.template import RequestContext
+from django.shortcuts import render_to_response
  # Create your views here.
 
+logger = logging.getLogger('development')
 
 class DonorListView(ListView):
     template_name = 'donors/donors_list.html'
@@ -16,5 +23,80 @@ class DonorCreateView(CreateView):
     form_class = DonorForms
     success_url = '/donor/'
 
-class SearchFormView(TemplateView):
+
+class SearchFormView(ListView):
     template_name = 'donors/donors_search.html'
+    model = Donor
+    success_url = '/donor_search/'
+    def get_queryset(self):
+        return Donor.objects.filter(hair_color=0)
+
+
+# def donorsearch(request):
+#     form = DonorSreachForm()
+#     if request.method == 'GET':
+#         return render_to_response(
+#             'donors_search.html',{'form':form},RequestContext(request)
+#         )
+#     elif request.method == 'POST':
+#         form = DonorSreachForm(request.POST)
+#         donors = Donor.objects.all()
+#         if form.is_valid():
+#             donors = donor.filter(Q(hair_color_contains = form.cleaned_data['hair_color'])|Q(height_contains = form.cleaned_data['height']))
+#             return render_to_response(
+#                 'donor_search.html',
+#                 {'form':form,'donors':donors},
+#                 RequestContext(request)
+#             )
+
+
+# class SearchFormView(ListView):
+#     paginate_by = 5
+#     template_name = 'donors/donors_search.html'
+#     model = Donor
+
+#     # セッションに検索フォームの値を渡す
+#     def donor(self,request,*args,**kwargs):
+#         form_value = [
+#             self.request.POST.get('hair_color',None)
+#         ]
+#         request.session['form_value'] = form_value
+
+#         # 検索時にページネーションに関連したエラーを防ぐ
+#         self.request.GET = self.request.GET.copy()
+#         self.request.GET.clear()
+        
+#         return self.get(request, *args, **kwargs)
+
+
+#     def get_context_data(self,**kwargs):
+#         context = super().get_context_data(**kwargs)
+
+#         # sessionに値がある場合、その値をセットする。（ページングしてもform値が変わらないように）
+#         hair_color = ''
+#         if 'form_value' in self.request.session:
+#             form_value = self.request.session['form_value']
+#             hair_color = form_value[0]
+
+#         default_data = {'hair _color':hair_color}
+
+#         test_form = DonorSreachForm(initial=default_data) #検索データ
+#         context['test_form'] = test_form
+
+#         return context
+
+#     def get_queryset(self):
+#         # sessionに値がある場合、その値でクエリ発行する。
+#         if 'form_value' in self.request.session:
+#             form_value = self.request.session['form_value']
+#             hair_color = form_value[0]
+
+#             #検索条件
+#             condition_hair_color = Q()
+
+#             if len(hair_color) != 0 and hair_color[0]:
+#                 condition_hair_color = Q(hair_color_icontains = hair_color)
+
+#             return Donor.objects.select_related().filter(condition_hair_color)
+#         else:
+#             return Donor.objects.none()
