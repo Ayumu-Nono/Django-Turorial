2f10032c347b9e97cd6e2078117f7483537cc37d
Ayumu-Nono <ayumu.bete@gmail.com>
2019-09-27 09:13:37 +0900

    Herokuデプロイ

--- astrowalker/astrowalker/articles/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def tags(querydict):
-    tags = querydict.get('tags1')
-
-    return "" if tags is None else tags
--- astrowalker/astrowalker/astrowalker/templates/context_processors.py
+++
@@ -1,5 +0,0 @@
-def common(request):
-    context = {}
-    context['tags'] = 0
-    # 変数宣言の仕方
-    return context
